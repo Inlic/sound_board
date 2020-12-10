114a4d798d929cc34eb65750c71d53b3ff17142e
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-09 19:51:15 -0700

    basic app structure

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ sounds.forEach(sound => {
   const btn = document.createElement('button')
   btn.classList.add('btn')
 
+  btn.addEventListener('click', () => {
+    stopSongs()
+    document.getElementById(sound).play()
+  })
+
   btn.innerText = sound
   document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+    song.pause()
+    song.currentTime = 0;
+  })
+}
